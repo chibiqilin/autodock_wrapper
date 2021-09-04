106c3b65e8e0d9369d271d70d56246fee2c53aa5
ChibiQilin <chibiqilin@gmail.com>
2021-09-03 22:46:36 -0400

    Added smiles support, changed input method.

--- advina.py
+++ advina.py
@@ -1,4 +1,5 @@
-def adock(receptor_name,
+def adock(receptor_input,
+        smiles,
         ligand_name,
         center_x=9.879,
         center_y=-13.774,
@@ -6,9 +7,13 @@ def adock(receptor_name,
         size_x=60,
         size_y=60,
         size_z=60,
-        vina='vina'
-        seed=None
-        cpu=1):
+        vina='vina',
+        seed=None,
+        cpu=1,
+        lig_dir = './ligand_files/',
+        out_dir = './output/',
+        log_dir = './log/',
+        conf_dir = './config/'):
 
     #Imports
     import os
@@ -18,23 +23,34 @@ def adock(receptor_name,
 
     timeout_duration = 600
 
-    pro_dir = './protein_files/'
-    lig_dir = './ligand_files/'
-    out_dir = './output/'
-    log_dir = './log/'
-    conf_dir = './config/'
+    #mkdir
+    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(conf_dir, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(lig_dir, exist_ok=True)
 
-    protein = pro_dir + receptor_name + '.pdbqt'
     ligand = lig_dir + ligand_name + '.pdbqt'
     output = out_dir + ligand_name + '_out.pdbqt'
     config = conf_dir + ligand_name + '.conf'
     log = log_dir + ligand_name + '_log.txt'
 
+    #Convert smiles
+    if not os.path.isfile(ligand):
+        with subprocess.Popen('obabel -:"' + smiles + '" -O ' + ligand + ' -h --gen3d' + ' > /dev/null 2>&1', \
+                shell=True, start_new_session=True) as proc:
+            try:
+                proc.wait(timeout=timeout_duration)
+            except subprocess.TimeoutExpired:
+                p = psutil.Process(proc.pid)
+                p.terminate()
+    else:
+        print(f'Ligand file: {ligand!r} already exists.')
+
     #Dock
-    if os.path.isfile(ligand):
+    if os.path.isfile(receptor_input):
         if not os.path.isfile(output):
             #Create conf files
-            conf = 'receptor = ' + protein + '\n' +\
+            conf = 'receptor = ' + receptor_input + '\n' +\
                     'ligand = ' + ligand + '\n' + \
                     'center_x = ' + str(center_x) + '\n' + \
                     'center_y = ' + str(center_y) + '\n' + \
@@ -43,14 +59,12 @@ def adock(receptor_name,
                     'size_y = ' + str(size_y) + '\n' + \
                     'size_z = ' + str(size_z) + '\n' + \
                     'out = ' + output + '\n' + \
-                    'seed = ' + str(seed) + '\n' + \
                     'cpu = ' + str(cpu)
             
-            #mkdir for output, conf files and logs
-            os.makedirs(out_dir, exist_ok=True)
-            os.makedirs(conf_dir, exist_ok=True)
-            os.makedirs(log_dir, exist_ok=True)
-            
+            if seed is not None:
+                conf += '\n' \
+                    'seed = ' + str(seed)
+
             with open(config, 'w') as f:
                 f.write(conf)
             with subprocess.Popen('' + vina + \
@@ -63,7 +77,6 @@ def adock(receptor_name,
                 except subprocess.TimeoutExpired:
                     p = psutil.Process(proc.pid)
                     p.terminate()
-
         result = None
         try:
             score = float("inf")
@@ -74,9 +87,11 @@ def adock(receptor_name,
                         score = min(score, float(new_score))
                 result = score
         except FileNotFoundError:
+            print('test')
             result = None
         
     else:
+        print(f'Protein file: {receptor_input!r} not found!')
         result = None
 
     return (result)
